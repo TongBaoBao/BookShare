3e88dfdfb036f1868d8401167269a6c9b00bc8a4
TongBaoBao <419715598@qq.com>
2017-03-12 08:58:09 +0800

    Update api_router.js

--- BookShare/server/api_router.js
+++ BookShare/server/api_router.js
@@ -3,5 +3,6 @@ const userApi = require('./api/user')
 
 const router = express.Router()
 router.post("/api/user/login",userApi.loginByUserName);
+router.post("/api/user/register",userApi.register);
 
 module.exports = router;
